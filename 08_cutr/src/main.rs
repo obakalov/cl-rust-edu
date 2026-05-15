1e28ef6a6fe6535a15878d0ace10a625b9b64c12
Oleksii <oleksii.bakalov@gmail.com>
2026-05-15 00:24:08 -0400

    Refactor `cutr` to add extraction helper functions and rework file handling logic

--- 08_cutr/src/main.rs
+++ 08_cutr/src/main.rs
@@ -1,6 +1,10 @@
-use anyhow::{bail, Result};
+use anyhow::{Result, bail};
 use clap::Parser;
-use std::io::Read;
+use clap::builder::Str;
+use csv::{ReaderBuilder, StringRecord};
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, Read};
 use std::num::NonZeroUsize;
 use std::ops::Range;
 
@@ -23,11 +27,13 @@ struct Args {
 
 #[derive(Debug, clap::Parser)]
 struct ArgsExtract {
-    #[arg(short = 'f', long = "fields", help = "Extract fields", value_name = "FIELDS", num_args(1..))]
+    #[arg(short = 'f', long = "fields", help = "Extract fields", value_name = "FIELDS", num_args(1..
+    ))]
     fields: Option<String>,
     #[arg(short = 'b', long = "bytes", help = "Extract bytes", value_name = "BYTES", num_args(1..))]
     bytes: Option<String>,
-    #[arg(short = 'c', long = "chars", help = "Extract characters", value_name = "CHARS", num_args(1..))]
+    #[arg(short = 'c', long = "chars", help = "Extract characters", value_name = "CHARS", num_args(1..
+    ))]
     chars: Option<String>,
 }
 
@@ -55,24 +61,21 @@ fn run(args: Args) -> Result<()> {
 
     let extract = get_extract(&args.extract);
 
-    args.files.iter().for_each(|file_name| {
-        let mut file = std::fs::File::open(file_name).unwrap_or_else(|e| {
-            eprintln!("Failed to open file {file_name}: {e}");
-            std::process::exit(1);
-        });
-        let mut file_content = String::new();
-
-        file.read_to_string(&mut file_content).unwrap_or_else(|e| {
-            eprintln!("Failed to read file {file_name}: {e}");
-            std::process::exit(1);
-        });
-
-        file_content.lines().for_each(|line| {
-            println!("{line}");
-        });
-    });
+    for file_name in &args.files {
+        match open_file(file_name) {
+            Err(e) => eprintln!("Failed to open file {file_name}: {e}"),
+            Ok(buffer) => todo!(),
+        }
+    }
 
     Ok(())
+}
+
+fn open_file(file_name: &str) -> Result<Box<dyn BufRead>> {
+    match file_name {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        name => Ok(Box::new(BufReader::new(std::fs::File::open(name)?))),
+    }
 }
 
 fn get_extract(extract: &ArgsExtract) -> Result<Extract> {
@@ -85,6 +88,43 @@ fn get_extract(extract: &ArgsExtract) -> Result<Extract> {
     } else {
         unreachable!()
     }
+}
+
+fn extract_fields(line: &StringRecord, field_pos: &[Range<usize>]) -> Vec<String> {
+    field_pos
+        .iter()
+        .flat_map(|range| range.clone())
+        .filter_map(|i| line.get(i))
+        .map(|s| s.to_string())
+        .collect()
+}
+
+fn extract_chars(line: &str, char_pos: &[Range<usize>]) -> String {
+    let letters = line.chars().collect::<Vec<char>>();
+    let len = letters.len();
+    char_pos
+        .iter()
+        .filter_map(|range| {
+            let clamped = range.start..range.end.min(len);
+            letters.get(clamped)
+        })
+        .flatten()
+        .collect::<String>()
+}
+
+fn extract_bytes(line: &str, byte_pos: &[Range<usize>]) -> String {
+    let bytes = line.as_bytes();
+    let len = bytes.len();
+    let result: Vec<u8> = byte_pos
+        .iter()
+        .filter_map(|range| {
+            let clamped = range.start..range.end.min(len);
+            bytes.get(clamped)
+        })
+        .flatten()
+        .copied()
+        .collect();
+    String::from_utf8_lossy(&result).into_owned()
 }
 
 fn parse_pos(raw_string: &str) -> Result<PositionList> {
@@ -115,7 +155,8 @@ fn parse_pos(raw_string: &str) -> Result<PositionList> {
 
 #[cfg(test)]
 mod unit_tests {
-    use super::parse_pos;
+    use super::{extract_bytes, extract_chars, extract_fields, parse_pos};
+    use csv::StringRecord;
 
     #[test]
     fn test_parse_pos_error_values() {
@@ -234,5 +275,35 @@ mod unit_tests {
         let res = parse_pos("15, 19-20");
         assert!(res.is_ok());
         assert_eq!(res.unwrap(), vec![14..15, 18..20]);
+    }
+
+    #[test]
+    fn test_extract_fields() {
+        let rec: StringRecord = StringRecord::from(vec!["Captain", "Sham", "12345"]);
+        assert_eq!(extract_fields(&rec, &[0..1]), &["Captain"]);
+        assert_eq!(extract_fields(&rec, &[1..2]), &["Sham"]);
+        assert_eq!(extract_fields(&rec, &[0..1, 2..3]), &["Captain", "12345"]);
+        assert_eq!(extract_fields(&rec, &[0..1, 3..4]), &["Captain"]);
+        assert_eq!(extract_fields(&rec, &[1..2, 0..1]), &["Sham", "Captain"]);
+    }
+
+    #[test]
+    fn test_extract_chars() {
+        assert_eq!(extract_chars("", &[0..1]), "".to_string());
+        assert_eq!(extract_chars("ábc", &[0..1]), "á".to_string());
+        assert_eq!(extract_chars("ábc", &[0..1, 2..3]), "ác".to_string());
+        assert_eq!(extract_chars("ábc", &[0..3]), "ábc".to_string());
+        assert_eq!(extract_chars("ábc", &[2..3, 1..2]), "cb".to_string());
+        assert_eq!(extract_chars("ábc", &[0..1, 1..2, 4..5]), "áb".to_string());
+    }
+
+    #[test]
+    fn test_extract_bytes() {
+        assert_eq!(extract_bytes("ábc", &[0..1]), "�".to_string());
+        assert_eq!(extract_bytes("ábc", &[0..2]), "á".to_string());
+        assert_eq!(extract_bytes("ábc", &[0..3]), "áb".to_string());
+        assert_eq!(extract_bytes("ábc", &[0..4]), "ábc".to_string());
+        assert_eq!(extract_bytes("ábc", &[3..4, 2..3]), "cb".to_string());
+        assert_eq!(extract_bytes("ábc", &[0..2, 5..6]), "á".to_string());
     }
 }
